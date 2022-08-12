46821b241ba1c2f20ae765e2d11d5467d3a458e2
BBudnicki <brandon@Budnicki.net>
2022-08-12 15:06:18 -0600

    recreated story map image loading bug

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Iframe url="https://storymaps.arcgis.com/stories/12f39d7a57e047c8a6274e61db8df21f" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Iframe from "./components/iframe.vue";
+
+@Component({
+  components: {
+    Iframe,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "node_modules/bootstrap/scss/bootstrap.scss";
+#app {
+  height: 100vh;
+}
+</style>
---
+++ src/components/iframe.vue
@@ -0,0 +1,66 @@
+<template lang="">
+  <div class="h-100 w-100">
+    <iframe
+      @load="setIframeLoaded()"
+      class="iframe-scale iframe-border-none btn-fade"
+      frameborder="0"
+      allow="fullscreen; geolocation;"
+      :class="{
+        'd-none': !loaded,
+      }"
+      :src="url"
+      title="Story Map"
+    ></iframe>
+    <div
+      class="flex-column h-100 text-center text-white justify-content-center"
+      :class="{ 'd-none': loaded, 'd-flex': !loaded }"
+    >
+      <div class="h1">{{ url }}</div>
+      <div class="mt-4">
+        <font-awesome-icon class="fa-2x" :icon="['fas', 'spinner']" spin />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  components: {},
+  data() {
+    return {
+      loaded: false,
+    };
+  },
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  watch: {},
+  computed: {
+  },
+  methods: {
+    setIframeLoaded() {
+      console.log("iframe loaded");
+      setInterval(() => {
+        this.loaded = true;
+      }, 0);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.iframe-scale {
+  width: 100%;
+  height: 100%;
+}
+
+.popup-iframe {
+  padding: 0;
+  height: 60vh;
+  width: 60vw;
+  z-index: 99999 !important;
+}
+</style>
